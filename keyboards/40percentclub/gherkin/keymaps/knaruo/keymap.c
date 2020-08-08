ac7ab63c182765f54985fae089533a06d09804b9
Kotaro Naruo <knaruo@gmail.com>
2020-08-09 00:10:10 +0900

    Completed NUM layer

--- keyboards/40percentclub/gherkin/keymaps/knaruo/keymap.c
+++ keyboards/40percentclub/gherkin/keymaps/knaruo/keymap.c
@@ -69,6 +69,8 @@ void ql_reset(qk_tap_dance_state_t *state, void *user_data);
 #define SFT_ENT     RSFT_T(KC_ENT)
 #define CTL_ESC     RCTL_T(KC_ESC)
 #define CTL_BSPC    RCTL_T(KC_BSPC)
+#define KC_J_BSLS   KC_INT1 /* JIS \_ */
+#define SFT_Z       LSFT_T(KC_Z)
 
 
 /***********************************************************
@@ -90,13 +92,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [CL_BASE] = LAYOUT_ortho_3x10_inv(
     TD(TD_Q_ESC),    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
     TD(TD_A_TAB),    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    CTL_BSPC,
-    KC_Z,    TD(TD_X_ZKHK),    KC_C,    KC_V,   FN1_SPC,  TD(TD_LAYER), KC_B,   KC_N,   KC_M,   SFT_ENT
+    SFT_Z,    TD(TD_X_ZKHK),    KC_C,    KC_V,   FN1_SPC,  TD(TD_LAYER), KC_B,   KC_N,   KC_M,   SFT_ENT
   ),
 
   [CL_NUM] = LAYOUT_ortho_3x10_inv(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-    _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______
+    /* |-/= |^/~ |\/| | */                                /* |@/` |;/+ |:* |[/{ |]/} */
+    KC_MINS,   KC_EQL,   KC_JYEN,   _______,   _______,   KC_LBRC, KC_SCLN, KC_QUOT, KC_RBRC, KC_BSLS,
+    KC_LSFT, _______, _______, _______, KC_DEL,  _______, KC_COMM, KC_DOT, KC_SLSH, KC_J_BSLS
   ),
 
   [CL_FN] = LAYOUT_ortho_3x10_inv(
