7fb1e9e571b424195414bd7ff2b6260684a966a1
Kotaro Naruo <knaruo@gmail.com>
2020-08-09 22:36:55 +0900

    Supported Shift on left side too

--- keyboards/40percentclub/gherkin/keymaps/knaruo/keymap.c
+++ keyboards/40percentclub/gherkin/keymaps/knaruo/keymap.c
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [CL_BASE] = LAYOUT_ortho_3x10_inv(
     TD(TD_Q_ESC),    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
     TD(TD_A_TAB),    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    CTL_BSPC,
-    KC_Z,    TD(TD_X_ZK_WIN),    KC_C,    KC_V,   FN1_SPC,  TD(TD_LAYER), KC_B,   KC_N,   KC_M,   SFT_ENT
+    SFT_Z,    TD(TD_X_ZK_WIN),    KC_C,    KC_V,   FN1_SPC,  TD(TD_LAYER), KC_B,   KC_N,   KC_M,   SFT_ENT
   ),
 
   [CL_NUM] = LAYOUT_ortho_3x10_inv(
