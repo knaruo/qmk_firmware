6d6891922e85cc7c44846a23e177c5172bf00da2
Kotaro Naruo <knaruo@gmail.com>
2020-08-10 23:16:02 +0900

    Supported Combo for comma, dot.

--- keyboards/40percentclub/gherkin/keymaps/knaruo/combo.c
+++ keyboards/40percentclub/gherkin/keymaps/knaruo/combo.c
@@ -6,13 +6,19 @@
 
 enum combo_event {
     KL_UNDS_COMBO,
+    KL_DOT_COMBO,
+    KL_COMM_COMBO,
 };
 
 const uint16_t PROGMEM unds_combo[] = {KC_N, KC_M, COMBO_END};
+const uint16_t PROGMEM dot_combo[] = {KC_K, KC_L, COMBO_END};
+const uint16_t PROGMEM comm_combo[] = {KC_I, KC_O, COMBO_END};
 
 // Register the combo action
 combo_t key_combos[COMBO_COUNT] = {
     [KL_UNDS_COMBO] = COMBO_ACTION(unds_combo),
+    [KL_DOT_COMBO] = COMBO_ACTION(dot_combo),
+    [KL_COMM_COMBO] = COMBO_ACTION(comm_combo),
 };
 
 // Called after a combo event is triggered
@@ -23,6 +29,20 @@ void process_combo_event(uint8_t combo_index, bool pressed) {
                 register_code16(KC_J_UNDS);
             } else {
                 unregister_code16(KC_J_UNDS);
+            }
+            break;
+        case KL_DOT_COMBO:
+            if (pressed) {
+                register_code(KC_DOT);
+            } else {
+                unregister_code(KC_DOT);
+            }
+            break;
+        case KL_COMM_COMBO:
+            if (pressed) {
+                register_code(KC_COMM);
+            } else {
+                unregister_code(KC_COMM);
             }
             break;
         default:
--- keyboards/40percentclub/gherkin/keymaps/knaruo/config.h
+++ keyboards/40percentclub/gherkin/keymaps/knaruo/config.h
@@ -1,5 +1,5 @@
 #pragma once
 
 #define TAPPING_TERM    175 // milliseconds
-#define COMBO_COUNT     1
+#define COMBO_COUNT     3
 #define COMBO_TERM      20
