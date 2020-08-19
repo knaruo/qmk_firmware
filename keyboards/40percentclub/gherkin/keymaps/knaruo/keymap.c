b006505895d28170a43e004d49ae6a8d4b0d1b64
Kotaro Naruo <knaruo@gmail.com>
2020-08-19 22:36:02 +0900

    Simplified Space key (unassigned layer change function)

--- keyboards/40percentclub/gherkin/keymaps/knaruo/keymap.c
+++ keyboards/40percentclub/gherkin/keymaps/knaruo/keymap.c
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [CL_BASE] = LAYOUT_ortho_3x10_inv(
     TD(TD_Q_ESC),    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    CTL_BSPC,
-    SFT_Z,    TD(TD_X_ZK_WIN),  KC_C,    KC_V,   FN_NUM_SPC,  TD(TD_LAYER), KC_B,   KC_N,   KC_M,   SFT_ENT
+    SFT_Z,    TD(TD_X_ZK_WIN),  KC_C,    KC_V,   KC_SPC,  TD(TD_LAYER), KC_B,   KC_N,   KC_M,   SFT_ENT
   ),
 
   [CL_NUM] = LAYOUT_ortho_3x10_inv(
