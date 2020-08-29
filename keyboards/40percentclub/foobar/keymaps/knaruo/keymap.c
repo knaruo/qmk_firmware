6240fdfcce038ac777a1ba9919ea00a888a3c299
knaruo <knaruo@gmail.com>
2020-08-29 22:15:16 +0900

    Updated SHIFT_Z to simple SHIFT on NUM layer.

--- keyboards/40percentclub/foobar/keymaps/knaruo/keymap.c
+++ keyboards/40percentclub/foobar/keymaps/knaruo/keymap.c
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     /* |-/= |^/~ |\/| | */                                /* |@/` |;/+ |:* |[/{ |]/} */
     KC_MINS,   KC_EQL,   KC_JYEN,   KC_LALT, KC_LBRC, KC_SCLN, KC_QUOT, KC_RBRC, KC_BSLS, _______,
-    _______, _______, _______, KC_COMM, _______,  _______, KC_DOT, KC_SLSH, KC_J_BSLS, _______
+    KC_LSFT, _______, _______, KC_COMM, _______,  _______, KC_DOT, KC_SLSH, KC_J_BSLS, _______
   ),
 
   [CL_FN] = LAYOUT_split(
