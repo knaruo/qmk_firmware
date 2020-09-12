d3b19bf33dce38ab3cd1d63e9fd0a64bc0ecbf79
knaruo <knaruo@gmail.com>
2020-09-12 15:06:32 +0900

    Applied the correct way to inverse the key map (USB port to left side)

--- keyboards/40percentclub/gherkin/keymaps/knaruo/config.h
+++ keyboards/40percentclub/gherkin/keymaps/knaruo/config.h
@@ -16,6 +16,15 @@
 
 #pragma once
 
+/* Make layout the right way:
+ *  - USB port on left side
+ *  - Switches facing the correct way
+ */
+#undef MATRIX_ROW_PINS
+#undef MATRIX_COL_PINS
+#define MATRIX_ROW_PINS { B6, B2, B3, B1, F7 }
+#define MATRIX_COL_PINS { D0, D4, C6, D7, E6, B4 }
+
 #define TAPPING_TERM    175 // milliseconds
 #define IGNORE_MOD_TAP_INTERRUPT
 #define COMBO_COUNT     5
--- keyboards/40percentclub/gherkin/keymaps/knaruo/keymap.c
+++ keyboards/40percentclub/gherkin/keymaps/knaruo/keymap.c
@@ -86,20 +86,20 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Base QWERTY key map */
-  [CL_BASE] = LAYOUT_ortho_3x10_inv(
+  [CL_BASE] = LAYOUT_ortho_3x10(
     TD(TD_Q_ESC),    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    CTL_BSPC,
     SFT_Z,    TD(TD_X_ZK_WIN),  KC_C,    KC_V,   FN_NUM_SPC,  TD(TD_LAYER), KC_B,   KC_N,   KC_M,   SFT_ENT
   ),
 
-  [CL_NUM] = LAYOUT_ortho_3x10_inv(
+  [CL_NUM] = LAYOUT_ortho_3x10(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     /* |-/= |^/~ |\/| | */                                /* |@/` |;/+ |:* |[/{ |]/} */
     KC_TAB,  KC_MINS,   KC_EQL,   KC_JYEN, KC_LBRC, KC_SCLN, KC_QUOT, KC_RBRC, KC_BSLS, CTL_BSPC,
     KC_LSFT, _______, KC_LALT, KC_COMM, _______,  _______, KC_DOT, KC_SLSH, KC_J_BSLS, _______
   ),
 
-  [CL_FN] = LAYOUT_ortho_3x10_inv(
+  [CL_FN] = LAYOUT_ortho_3x10(
     KC_F11, KC_F2,   KC_F3, KC_F4,  KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
     KC_TAB,  KC_F12,  _______, _______, KC_PSCR, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, LALT(LCTL(KC_DEL)),
     KC_LSFT, _______, _______, KC_HENK, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
