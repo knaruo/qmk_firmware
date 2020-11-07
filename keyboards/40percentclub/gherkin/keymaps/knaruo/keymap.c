dcf8d806be96725438a93ec43fade2fef21618c8
knaruo <knaruo@gmail.com>
2020-11-07 17:19:49 +0900

    Knaruo gherkin 4 (#5)
    * Simplified the tap dance codes.
    
    * Removed the combo keys that are not frequently used
    
    * Assigned KC_ZKHK on the NUM layer and disabled the KC_ZKHK by combo keys
    
    All combo keys are now not effectively used - so combo key feature is disabled from the config.h/rules.mk
    
    * merge foobar/knaruo keymap to gherkin

--- keyboards/40percentclub/gherkin/keymaps/knaruo/keymap.c
+++ keyboards/40percentclub/gherkin/keymaps/knaruo/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     /* |-/= |^/~ |\/| | */                                /* |@/` |;/+ |:* |[/{ |]/} */
     KC_TAB,  KC_MINS,   KC_EQL,   KC_JYEN, KC_LBRC, KC_SCLN, KC_QUOT, KC_RBRC, KC_BSLS, CTL_BSPC,
-    KC_LSFT, _______, KC_LALT, KC_COMM, _______,  _______, KC_DOT, KC_SLSH, KC_J_BSLS, _______
+    KC_LSFT, KC_ZKHK, KC_LALT, KC_COMM, _______,  _______, KC_DOT, KC_SLSH, KC_J_BSLS, _______
   ),
 
   [CL_FN] = LAYOUT_ortho_3x10(
--- keyboards/40percentclub/gherkin/keymaps/knaruo/rules.mk
+++ keyboards/40percentclub/gherkin/keymaps/knaruo/rules.mk
@@ -1,5 +1,4 @@
 TAP_DANCE_ENABLE = yes
-COMBO_ENABLE = yes
 RGBLIGHT_ENABLE = yes
 
-SRC += custom_tap.c td_qesc.c combo.c
+SRC += custom_tap.c td_qesc.c
