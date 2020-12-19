e6cd2c38948d4e30bfe217cd1845bb17983b6fae
knaruo <knaruo@gmail.com>
2020-12-19 14:47:50 +0900

    basic layout is done

--- keyboards/pisces/pisces.h
+++ keyboards/pisces/pisces.h
@@ -26,10 +26,16 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
+
 #define LAYOUT( \
-    k00, k01, k02, \
-      k10,  k12    \
+    L00, L01, L02, L03, L04, L05,           R00, R01, R02, R03, R04, R05, \
+    L10, L11, L12, L13, L14, L15,           R10, R11, R12, R13, R14, R15, \
+    L20, L21, L22, L23, L24, L25, L26, R26, R20, R21, R22, R23, R24, R25  \
 ) { \
-    { k00, k01,   k02 }, \
-    { k10, KC_NO, k12 }  \
+    { L00, L01, L02, L03, L04, L05, KC_NO }, \
+    { L10, L11, L12, L13, L14, L15, KC_NO }, \
+    { L20, L21, L22, L23, L24, L25, L26 }, \
+    { R05, R04, R03, R02, R01, R00, KC_NO }, \
+    { R15, R14, R13, R12, R11, R10, KC_NO }, \
+    { R25, R24, R23, R22, R21, R20, R26 } \
 }
