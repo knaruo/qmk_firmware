c1f0e1b7ece1bedb3b2e4c6c3cc1ca02716ea9c8
knaruo <knaruo@gmail.com>
2020-12-19 21:17:28 +0900

    pisces/default - the first version built

--- keyboards/pisces/keymaps/default/keymap.c
+++ keyboards/pisces/keymaps/default/keymap.c
@@ -69,23 +69,23 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Base QWERTY key map */
-  [CL_BASE] = LAYOUT_split(
-    TD(TD_Q_ESC),    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    CTL_BSPC,
-    SFT_Z,    LWIN_T(KC_X),  KC_C,    KC_V,   SPC_LY_NUM,  DEL_LY_FN, KC_B,   KC_N,   KC_M,   SFT_ENT
+  [CL_BASE] = LAYOUT(
+    KC_TAB, TD(TD_Q_ESC),    KC_W,    KC_E,    KC_R,    KC_T,  /* -- */  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_DEL,
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    /* -- */  KC_H,    KC_J,    KC_K,    KC_L,    KC_COMM,    CTL_BSPC,
+    KC_LSFT, KC_Z,    LWIN_T(KC_X),  KC_C,    KC_V,   SPC_LY_NUM,  KC_LALT, KC_BSPC, DEL_LY_FN, KC_B,   KC_N,   KC_M,   KC_DOT,       SFT_ENT
   ),
 
-  [CL_NUM] = LAYOUT_split(
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+  [CL_NUM] = LAYOUT(
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,  /* -- */  KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
     /* |-/= |^/~ |\/| | */                                /* |@/` |;/+ |:* |[/{ |]/} */
-    KC_TAB,  KC_MINS,   KC_EQL,   KC_JYEN, KC_LBRC, KC_SCLN, KC_QUOT, KC_RBRC, KC_BSLS, CTL_BSPC,
-    KC_LSFT, KC_ZKHK, KC_LALT, KC_COMM, _______,  _______, KC_DOT, KC_SLSH, KC_J_BSLS, _______
+    _______, KC_TAB,  KC_MINS,   KC_EQL,   KC_JYEN, KC_LBRC, /* -- */ KC_SCLN, KC_QUOT, KC_RBRC, KC_BSLS, CTL_BSPC, _______,
+    _______, KC_LSFT, KC_ZKHK, KC_LALT, KC_COMM, _______,  _______, _______, _______, KC_DOT, KC_SLSH, KC_J_BSLS, _______, _______
   ),
 
-  [CL_FN] = LAYOUT_split(
-    KC_F11, KC_F2,   KC_F3, KC_F4,  KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
-    KC_TAB,  KC_F12,  _______, _______, KC_PSCR, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, LALT(LCTL(KC_DEL)),
-    KC_LSFT, _______, _______, KC_HENK, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+  [CL_FN] = LAYOUT(
+    _______, KC_F11, KC_F2,   KC_F3, KC_F4,  KC_F5,  /* -- */ KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, _______,
+    _______, KC_TAB,  KC_F12,  _______, _______, KC_PSCR, /* -- */ KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, LALT(LCTL(KC_DEL)), _______,
+    _______, KC_LSFT, _______, _______, KC_HENK, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______
   ),
 
 };
